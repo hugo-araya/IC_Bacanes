d094d8ab3332165d518d1f6765ce0b7df094d262
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2024-04-18 20:22:55 -0400

    s

--- interes.py
+++ interes.py
@@ -1,5 +1,5 @@
 # Entero cap_invertir
 # Real ganancia
 cap_invertir = int(input('Ingrese el monto a invertir: '))
-interes = int(input('Ingrese el interes: '))
+
 print('Para un interes del',interes,'% su ganancia es: ',cap_invertir * (interes / 100))
